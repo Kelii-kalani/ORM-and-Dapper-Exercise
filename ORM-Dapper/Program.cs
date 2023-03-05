4ca470c6f5ce516bd08387515cf743b37914a1ac
Michael <keliikalani87@gmail.com>
2023-03-04 17:32:39 -0800

    initial commit

--- ORM-Dapper/Program.cs
+++ ORM-Dapper/Program.cs
@@ -18,20 +18,50 @@ namespace ORM_Dapper
             string connString = config.GetConnectionString("DefaultConnection");
 
             IDbConnection conn = new MySqlConnection(connString);
+            #region department region
+            //var departmentRepo = new DapperDepartmentRepository(conn);
 
-            var departmentRepo = new DapperDepartmentRepository(conn);
+            //departmentRepo.InsertDepartment("mikes new department");
 
-            departmentRepo.InsertDepartment("mikes new department");
+            // var departments = departmentRepo.GetAllDepartments();
 
-            var departments =departmentRepo.GetAllDepartments();
+            //foreach (var department in departments)
+            //{
+            // Console.WriteLine($"Dept.ID: {department.DepartmentID}");
+            //Console.WriteLine($"Dept.Name: {department.Name}");
+            // Console.WriteLine();
+            //Console.WriteLine();
+            //}
+            #endregion
+            #region Product region
+            var productRepo = new DapperProuctRepository(conn);
 
-            foreach (var department in departments)
+            //var productToUpdate = productRepo.GetProduct(940);
+
+            //productToUpdate.Name = "UPDATED!!!";
+            //productToUpdate.Price = 10.99;
+            //productToUpdate.CategoryID = 1;
+            //productToUpdate.OnSale = false;
+            //productToUpdate.StockLevel = 1000;
+
+            //productRepo.UpdateProduct(productToUpdate);
+
+            productRepo.DeleteProduct(940);
+
+            var products = productRepo.GetAllProducts();
+
+            foreach (var product in products)
             {
-                Console.WriteLine(department.DepartmentID);
-                Console.WriteLine(department.Name);
-                Console.WriteLine(); 
+                Console.WriteLine($"Product ID: {product.ProductID}");
+                Console.WriteLine($"Product name: {product.Name}");
+                Console.WriteLine($"Product price: {product.Price}");
+                Console.WriteLine($"Product categoryID: {product.CategoryID}");
+                Console.WriteLine($"Product on sale: {product.OnSale}");
+                Console.WriteLine($"Product stock level: {product.StockLevel}");
+                Console.WriteLine();
                 Console.WriteLine();
             }
+            #endregion
         }
     }
 }
